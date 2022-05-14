0972a81aadedcf49a693f0f4e865e70a708edb83
OzzyOuzo <sundayozzy@gmail.com>
2022-05-14 10:45:33 +0300

    added smpl chunk informations for ADPCM sample loop management

--- utils/wav2adpcm/wav2adpcm.c
+++ utils/wav2adpcm/wav2adpcm.c
@@ -13,11 +13,15 @@
     handle stereo (though the stereo is very likely KOS specific
     since we make no effort to interleave it). Please see README.GPL
     in the KOS docs dir for more info on the GPL license.
+
+
+    Added smpl chunk informations for sample loop management by Ozzy Ouzo.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
 
 static int diff_lookup[16] = {
@@ -184,10 +188,19 @@ struct wavhdr_t {
 
 int wav2adpcm(const char *infile, const char *outfile) {
     struct wavhdr_t wavhdr;
+    char dID[4];
+    uint32_t smplHeader[9];
+    uint32_t i, len, loopType = 0, start, temp, startLoop = 0, endLoop = 0;
+
+    uint32_t nSampleLength = 0;//, nSampleBlockAlign = 0;
+    bool have_fmt = false;
+    bool haveLoop = false;
+
+
     FILE *in, *out;
-    size_t pcmsize, adpcmsize;
-    short *pcmbuf;
-    unsigned char *adpcmbuf;
+    size_t pcmsize=0, adpcmsize=0;
+    short *pcmbuf=NULL;
+    unsigned char *adpcmbuf=NULL;
 
     in = fopen(infile, "rb");
 
@@ -196,36 +209,134 @@ int wav2adpcm(const char *infile, const char *outfile) {
         return -1;
     }
 
-    if(fread(&wavhdr, sizeof(wavhdr), 1, in) != 1) {
+    //load RIFF header...
+    if (fread(&wavhdr.hdr1[0], 4, 1, in) != 1 || fread(&wavhdr.totalsize, 4, 1, in) != 1 || fread(&wavhdr.hdr2[0], 4, 1, in) != 1) {
         fprintf(stderr, "Cannot read header.\n");
         fclose(in);
         return -1;
     }
 
-    if(memcmp(wavhdr.hdr1, "RIFF", 4)
-            || memcmp(wavhdr.hdr2, "WAVEfmt ", 8)
-            || memcmp(wavhdr.hdr3, "data", 4)
-            || wavhdr.hdrsize != 0x10
-            || wavhdr.format != 1
-            || (wavhdr.channels != 1 && wavhdr.channels != 2)
-            || wavhdr.bits != 16) {
+    if(memcmp(&wavhdr.hdr1[0], "RIFF", 3) || memcmp(&wavhdr.hdr2[0], "WAVE", 4)) {
         fprintf(stderr, "Unsupported format.\n");
         fclose(in);
         return -1;
     }
 
-    pcmsize = wavhdr.datasize;
+    for (;;) {
 
-    adpcmsize = pcmsize / 4;
-    pcmbuf = malloc(pcmsize);
-    adpcmbuf = malloc(adpcmsize);
+        for (i = 0; i < 4; i++) {
+            fread(&dID[i], 1, 1, in);
+        }
+        fread(&len, 4, 1, in);
 
-    if(fread(pcmbuf, pcmsize, 1, in) != 1) {
-        fprintf(stderr, "Cannot read data.\n");
-        fclose(in);
-        return -1;
+        /* Skipped intentionally bcoz we read multiple chunks headers
+        if (hdrSize != 0x10) {
+            fprintf(stderr, "Unsupported format.\n");
+            fclose(in);
+            return -1;
+        }
+        */
+
+        if (feof(in)) {
+            fprintf(stderr, "Invalid truncated WAVE header.\n");
+            fclose(in);
+            return -1;
+        }
+
+        start = ftell(in);
+
+        if (!memcmp(dID, "smpl", 4)) {
+            /* skipping Sampler Chunk header */
+            for (i = 0; i < 9; i++) {
+                fread(&temp, 4, 1, in);
+            }
+
+            fread(&temp, 4, 1, in);
+            fread(&loopType, 4, 1, in);
+            fread(&startLoop, 4, 1, in);
+            fread(&endLoop, 4, 1, in);
+            fread(&temp, 4, 1, in);
+            fread(&temp, 4, 1, in);
+
+            if (loopType == 0 && endLoop > 0) {
+                haveLoop = true;
+            }
+            if (startLoop > nSampleLength) {
+                startLoop = 0;
+            }
+            if (endLoop > nSampleLength) {
+                endLoop = nSampleLength;
+            }
+            break;
+        }
+        else {
+            if (!memcmp(dID, "fmt ", 4)) {
+                fread(&wavhdr.format, 2, 1, in);
+                fread(&wavhdr.channels, 2, 1, in);
+                fread(&wavhdr.freq, 4, 1, in);
+                fread(&wavhdr.byte_per_sec, 4, 1, in);
+                fread(&wavhdr.blocksize, 2, 1, in);
+                fread(&wavhdr.bits, 2, 1, in);
+
+                if (have_fmt || ((wavhdr.channels != 1) && (wavhdr.channels != 2)) || wavhdr.format != 1) {
+                    fprintf(stderr, "Unsupported format.\n");
+                    fclose(in);
+                    return -1;
+                }
+                have_fmt = true;
+            }
+            else {
+                if (!memcmp(dID, "data", 4)) {
+                    if (!have_fmt) {
+                        fprintf(stderr, "Unsupported format.\n");
+                        fclose(in);
+                        return -1;
+                    }
+                    nSampleLength = len >> 1;
+                    //nSampleBlockAlign = wavhdr.blocksize;
+                    if (wavhdr.blocksize != 2) {
+                        fprintf(stderr, "Unsupported format.\n");
+                        fclose(in);
+                        return -1;
+                    }
+
+                    pcmsize = len;
+
+                    adpcmsize = pcmsize / 4;
+                    pcmbuf = malloc(pcmsize);
+                    adpcmbuf = malloc(adpcmsize);
+
+                    if (fread(pcmbuf, pcmsize, 1, in) != 1) {
+                        fprintf(stderr, "Cannot read data.\n");
+                        fclose(in);
+                        return -1;
+                    }
+
+                }
+            }
+        }
+        //onto next block 
+        fseek(in, start + len, SEEK_SET);
+        if (feof(in))
+            break;
     }
+
     fclose(in);
+
+    /* adding straight fmt chunk ID... */
+    strncpy(&wavhdr.hdr2[4], "fmt ",4);
+    wavhdr.hdrsize = 0x10;
+
+    /* adding data chunk ID... */
+    strncpy(&wavhdr.hdr3[0], "data",4);
+    wavhdr.datasize = adpcmsize;
+
+    /* no loop ? default looping. */
+    if (!endLoop) {
+        startLoop = 0;
+        endLoop = nSampleLength;
+        haveLoop = false;
+    }
 
     if(wavhdr.channels == 1) {
         pcm2adpcm(adpcmbuf, pcmbuf, pcmsize);
@@ -244,14 +355,29 @@ int wav2adpcm(const char *infile, const char *outfile) {
     wavhdr.totalsize = wavhdr.datasize + sizeof(wavhdr) - 8;
 
     out = fopen(outfile, "wb");
-    if(fwrite(&wavhdr, sizeof(wavhdr), 1, out) != 1
-            || fwrite(adpcmbuf, adpcmsize, 1, out) != 1) {
+    if(fwrite(&wavhdr, sizeof(wavhdr), 1, out) != 1 || fwrite(adpcmbuf, adpcmsize, 1, out) != 1) {
         fprintf(stderr, "Cannot write ADPCM data.\n");
         fclose(out);
         return -1;
     }
-    fclose(out);
 
+    /* Adding an extra chunk of informations for sample loop management*/
+    if (haveLoop) {
+        
+        strncpy(dID, "smpl", 4);
+        len = 0x3c;
+        /* dummy smpl header*/
+        memset(smplHeader, 0, 9 * sizeof(uint32_t));
+        temp = 0;
+
+        if (fwrite(dID, 4, 1, out) != 1 || fwrite(&len, 4, 1, out) != 1 || fwrite(smplHeader, 9 * sizeof(uint32_t), 1, out) != 1  || fwrite(&temp, 4, 1, out) != 1 || fwrite(&loopType, 4, 1, out) != 1 || fwrite(&startLoop, 4, 1, out) != 1 || fwrite(&endLoop, 4, 1, out) != 1 || fwrite(&temp, 4, 1, out) != 1 || fwrite(&temp, 4, 1, out) != 1) {
+            fprintf(stderr, "Cannot write smpl chunk data.\n");
+            fclose(out);
+            return -1;
+        }
+    }
+
+    fclose(out);
     return 0;
 }
 
